949cf1d53e56773b069f9b5868124aa769611e02
nariascataldi <nestorariascataldi@gmail.com>
2022-02-05 12:02:10 -0300

    first exercise in progress 32 the 35

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,9 +68,9 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   //.length
- var a = (str1);
- var b = (str2);
- if (a.length == b.length){
+ var caracteresA = (str1);
+ var caracteresB = (str2);
+ if (caracteresA.length == caracteresB.length){
    return true;
  } return false;
 }
@@ -163,47 +163,59 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (0 < numero) {
+    return 'Es positivo';    
+  } else if (numero < 0) {
+    return 'Es negativo';
+  }
+  return false;
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var concatenar = str + '!'
+  return concatenar;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+ var concatenar = nombre + ' ' + apellido
+ return concatenar; 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var hola = `Hola ${nombre}!`;
+  return hola;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = alto * ancho;
+  return area;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+var perimetroCuadrado = lado * 4;
+return perimetroCuadrado;   
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+var triangulo = (base * altura) / 2;
+return triangulo;
 }
 
 
@@ -211,7 +223,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var tasaDeCambioEuroADolar = 1.20;
+  var cambio = euro * tasaDeCambioEuroADolar;
+  return cambio;
 }
 
 
@@ -221,7 +235,11 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  var caracteres = letra;
+  var volacales = (a, e, i, o, u);
+  if (caracteres.length > 1){
+    return console.log('Dato incorrecto');
+  }
 }
 
 
