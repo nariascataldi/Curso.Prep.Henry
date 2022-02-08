bd0813026411c79bca6f7419a6b5f52cea70bfd2
nariascataldi <nestorariascataldi@gmail.com>
2022-02-08 17:38:58 -0300

    check operadoresLogicos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -132,13 +132,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
   if (!(numero % 3)){
-    return 'fizz';
+   if (!(numero % 5)) {
+     return 'fizzbuzz';
+    }else{
+      return 'fizz';
+    }
   }else if (!(numero % 5)){
-    return 'buzz';
-  }else if (!(numero % 3 && numero % 5)){
-    return 'fizzbuzz';
-  }else{
-    return numero;
+     return 'buzz';
+  }else {
+     return numero;
   }
 }
 
@@ -149,6 +151,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+if ((num1 === 0 || num2 === 0 || num3 === 0)){
+    return 'Error';
+}else if ((num3 && num2 < num1) && num1 >= 0){
+  return 'Número 1 es mayor y positivo';
+}else if (0 > num1 || 0 > num2 || 0 > num3){
+  return 'Hay negativos';
+}else if (num3 > (num1 && num2)) {
+  return num3++;
+}else {
+  return false;
+}
 }
 
 function esPrimo(numero) {
