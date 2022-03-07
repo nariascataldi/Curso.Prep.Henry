5acf4b5ccb4dcce15dad646ff41a6e0d1488d9cc
nariascataldi <nestorariascataldi@gmail.com>
2022-03-07 15:10:15 -0300

    extra 2 of 8

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -31,6 +31,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  var o = {};
+  for (let i = 0; i < string.length; i++){
+    if(o.hasOwnProperty(string[i])){
+      o[string[i]] = o[string[i]] + 1;
+    }else{
+      o[string[i]] = 1
+    }
+  }return o;
 }
 
 
