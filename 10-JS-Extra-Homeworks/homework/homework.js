37a4f12989ccd3fa964700ade5e1010ff9ee4a39
Nestor <nestorariascataldi@gmail.com>
2022-03-12 10:02:55 -0300

    home

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -81,6 +81,7 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  
 }
 
 
